2b6d9d4f898cd013d833a2ea6ef045a1bbb09ab0
anhhungtocdo36 <loctranthanh36@gmail.com>
2017-11-20 23:08:00 +0700

    remove sending slack

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var express = require('express');
-var Slack = require('slack.seed');
+//var Slack = require('slack.seed');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-webhookUri = "https://hooks.slack.com/services/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";
+//webhookUri = "https://hooks.slack.com/services/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";
 
-slack = new Slack();
-slack.setWebhook(webhookUri);
+//slack = new Slack();
+//slack.setWebhook(webhookUri);
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -17,13 +17,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
-  slack.webhook({
+  /*slack.webhook({
     channel: "#sensor",
     username: "ESP8266",
     text: "This is posted to #sensor and comes from a bot named ESP8266."
   }, function(err, response) {
     console.log(response);
-  });
+  });*/
 });
 
 app.listen(app.get('port'), function() {
