911b0973a764f3adfbc3d19c1172b71cea7f0e05
anhhungtocdo36 <loctranthanh36@gmail.com>
2017-11-21 00:49:58 +0700

    try send to slack

--- index.js
+++ index.js
@@ -14,19 +14,6 @@ client.on('connect', function () {
   client.publish('chrome_test', 'Hello mqtt');
 });
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-  slack.webhook({
-    channel: "#sensor",
-    username: "ESP8266",
-    text: message.toString(),
-  }, function(err, response) {
-    console.log(response);
-  });
-  client.end();
-});
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -54,4 +41,17 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  slack.webhook({
+    channel: "#sensor",
+    username: "ESP8266",
+    text: message.toString(),
+  }, function(err, response) {
+    console.log(response);
+  });
+  client.end();
 });
